8a77cd1aced6273e802c6a10c56965e9c0a3f647
Jorge <jepers2021@gmail.com>
2021-03-26 22:12:17 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A133590.Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 9");
+            Console.WriteLine("Dada una frase por el usuario, el programa presenta la misma frase en mayúscula.");
+            Console.Write("Por favor, ingrese una frase: ");
+            string frase = Console.ReadLine();
+            Console.WriteLine(frase.ToUpperInvariant());
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
